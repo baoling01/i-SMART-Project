b6c037659cb9696f773744757e1d0ca029a7232d
baoling01 <fbaoling01@gmail.com>
2025-04-11 09:55:25 +0800

    update mmu logo that align with the title

--- app.py
+++ app.py
@@ -14,14 +14,23 @@ import json
 # st.set_page_config(page_title="MMU Program Recommendation", page_icon="🛢️", layout="wide")
 st.set_page_config(page_title="Program Recommendation", page_icon="https://4.bp.blogspot.com/-_EMlBTSVU6E/Tha_OAK8nMI/AAAAAAAAACY/azBH7qFTRlg/s1600/mmu-logo_m.jpg", layout="wide")
 
-# Create a row with two columns
-col1, col2 = st.columns([1, 10])  # Adjust the ratio as needed
+st.markdown(
+    """
+    <div style="display: flex; align-items: center;">
+        <img src="https://cdforum.lne.st/wp-content/uploads/sites/61/2022/03/MMU-New-Secondary-logo.png" alt="MMU Logo" style="width: 120px; margin-right: 20px;">
+        <h1 style="margin: 0;">MMU Program Recommendation System</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
-with col1:
-    st.image("mmu_logo.png", width=100)  # Replace with your logo path
+# # Create a row with two columns
+# col1, col2 = st.columns([1, 10])  # Adjust the ratio as needed
 
-with col2:
-    st.title("MMU Program Recommendation System")
+# with col1:
+#     st.image("mmu_logo.png", width=100)
+# with col2:
+#     st.title("MMU Program Recommendation System")
 
 # Set path to Tesseract
 pytesseract.pytesseract.tesseract_cmd = r"/mnt/c/Program Files/Tesseract-OCR/tesseract.exe"  # For WSL (Windows Subsystem for Linux)
